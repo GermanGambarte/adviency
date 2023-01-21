33d35e33b4d48e320e01d7da8777959948775968
GermanGambarte <germangambarte@gmail.com>
2023-01-21 14:28:08 -0300

    finish: day 1

---
+++ react/day-1/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="gifts">
+      <main className="gifts-container">
+        <h1 className="gifts-title">Lista de Regalos</h1>
+        <ul className="gifts-list">
+          <li className="gifts-item">Regalo 1</li>
+          <li className="gifts-item">Regalo 2</li>
+          <li className="gifts-item">Regalo 3</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
