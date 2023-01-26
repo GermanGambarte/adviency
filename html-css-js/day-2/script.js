0ca0230d2a74201d9301701f797a0693ddd235fb
German Gambarte <germangabmarte@gmail.com>
2023-01-26 17:01:19 -0300

    add: DocumentFragment

--- html-css-js/day-2/script.js
+++ html-css-js/day-2/script.js
@@ -2,6 +2,8 @@ const $form = document.querySelector('.form')
 const $input = document.querySelector('.form-input')
 const $list = document.querySelector('.list')
 
+const fragment = new DocumentFragment()
+
 const initialValue = [
   'regalo X',
   'regalo Y',
@@ -12,19 +14,22 @@ const createGift = (gift) =>{
   const newGift = document.createElement('li')
   newGift.classList = 'list-item'
   newGift.textContent = gift
-  return newGift
+  fragment.appendChild(newGift)
 }
 
 const addInitialGifts = () => {
-  initialValue.forEach(gift => $list.appendChild(createGift(gift)))
+  initialValue.forEach(gift => createGift(gift))
+  $list.append(fragment)
 }
 
 const handleSubmit = (evt)=>{
   evt.preventDefault()
-  const gift = evt.target.newGift.value
-  $list.insertBefore(newItem,$list.firstChild)
-  evt.target.newGift.value = ''
- return 
+  const gift = $input.value
+  if(!gift) return $input.classList.add('form-input__error')
+  $input.classList.remove('form-input__error')
+  createGift(gift)
+  $list.insertBefore(fragment,$list.firstChild)
+  $input.value = ''
 }
 
 document.addEventListener('DOMContentLoaded', addInitialGifts)
