9aac28140c535dca246d2a1ff80d036ed537d711
German Gambarte <germangabmarte@gmail.com>
2023-01-26 23:01:44 -0300

    feature: add new gift

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,59 @@
-import "./App.css";
+import { useState } from 'react'
 
-function App() {
+import './App.css'
+
+function App () {
+  const [gifts, setGifts] = useState(['regalo 1', 'regalo 2', 'regalo 3'])
+  const [gift, setGift] = useState('')
+
+  const handleChange = (e) => {
+    setGift(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setGifts([
+      gift,
+      ...gifts
+    ])
+    setGift('')
+  }
+
   return (
     <div className="gifts">
       <main className="gifts-container">
         <h1 className="gifts-title">Lista de Regalos</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <label className='sr-only' htmlFor="newGift">Agregar Regalo</label>
+          <input
+          required
+          className='form-input form-input__error'
+            name="newGift"
+            type="text"
+            value={gift}
+            onChange={handleChange}
+          />
+          <button className='form-btn__submit' type='submit'>
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={1.5}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </form>
         <ul className="gifts-list">
-          <li className="gifts-item">Regalo 1</li>
-          <li className="gifts-item">Regalo 2</li>
-          <li className="gifts-item">Regalo 3</li>
+          {gifts.map((g) => <li key={g}>{g}</li>)}
         </ul>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
